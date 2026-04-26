a29bae3aa9866bcf1c5989698ae9a6da3610fb48
Krishna Vamsi Pinninti <krishpinninti789@gmail.com>
2026-04-26 20:35:42 +0530

    added max sub

--- MaxSubarraySum.py
+++ MaxSubarraySum.py
@@ -1,27 +1,11 @@
-class Solution(object):
-    def maximumSubarraySum(self, nums, k):
-        from collections import defaultdict
+def maxSubArray(nums):
         
-        freq = defaultdict(int)
-        w_sum = 0
-        max_sum = 0
-        left = 0
-        
-        for right in range(len(nums)):
-            # expand window
-            w_sum += nums[right]
-            freq[nums[right]] += 1
-            
-            # shrink if window > k
-            if right - left + 1 > k:
-                w_sum -= nums[left]
-                freq[nums[left]] -= 1
-                if freq[nums[left]] == 0:
-                    del freq[nums[left]]
-                left += 1
-            
-            # valid window check
-            if right - left + 1 == k and len(freq) == k:
-                max_sum = max(max_sum, w_sum)
-        
-        return max_sum
+    current_sum =0
+    max_sum = float("-inf")
+    for x in range(len(nums)):
+        current_sum = max(nums[x],current_sum+nums[x])
+        max_sum = max(max_sum,current_sum)
+    return max_sum
+
+ls = list(map(int,input("Enter numbers: ").split()))
+print(maxSubArray(ls))
